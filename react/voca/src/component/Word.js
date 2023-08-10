131bac2a37401818f80233bf4fc0c8902f49f3ca
won <71067307+won195@users.noreply.github.com>
2023-08-10 20:29:11 +0900

    lesson14

--- react/voca/src/component/Word.js
+++ react/voca/src/component/Word.js
@@ -1,6 +1,7 @@
 import { useState } from "react-router-dom";
 
-export default function Word({ word }){
+export default function Word(props){
+  const [word, setWord] = useState();
   const [isShow, setIsShow] = useState(false);
   const [isDone, setIsDone] = useState(word.isDone);
 
@@ -9,7 +10,29 @@ export default function Word({ word }){
   }
 
   function toggleDone(){
-    setIsDone(!isShow);
+    // setIsDone(!isShow);
+    fetch(`http://localhost:3001/words/${word.id}`,{
+      method : 'PUT',
+      headers :{
+        'Content-Type' : 'application/json',
+      },
+      body : JSON.stringify({
+        ...word,
+        isDone : !isDone
+      }),
+    }).then(res => {
+      if(res.ok) {
+        setIsDone(!isDone);
+      }
+    });
+  }
+
+  function del() {
+    if(window.confirm('삭제 하시겠습니까?')){
+      fetch(`http://localhost:3001/words/${word.id}`,{
+        method : 'DELETE',
+      });
+    }
   }
 
   return (          
@@ -21,7 +44,7 @@ export default function Word({ word }){
       <td>{isShow && word.kor}</td>
       <td>
         <button onClick={toggleShow}>뜻 보기{isShow ? '숨기기' : '보기'}</button>
-        <button class="btn_del">삭제</button>
+        <button className="btn_del">삭제</button>
       </td>
     </tr>
   )
